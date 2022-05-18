c72218c4faa6a94f42bf34e970ba503935c5a977
hassanplz <hassan.albujasim@ucdconnect.ie>
2022-05-18 16:10:33 +0100

    1.3 course infromation step3: using objects

--- part1/courseinfo/README.md
+++ part1/courseinfo/README.md
@@ -1,8 +1,59 @@
 # par1
-Introduction to React. 1b:
+
+Introduction to React. 
+
+1a:
+
     - A Component
+
     - JSX
+
     - Multiple Components
+
     - props: passing data to components.
+
     - notes.
 
+
+1b: 
+
+    - Variables: use let and const.
+
+    - Arrays: use immutable data structures. use functional programming methods such as ``concat``(creates a new array with new items) and not ``push``.
+
+    use of ``map.()`` function is frequently useful in React. snippet: 
+    ```const t = [1, 2, 3]
+    const m1 = t.map(value => value * 2)
+    console.log(m1)   // [2, 4, 6] is printed``` 
+
+    destructuring assignment is useful. snippet:
+    ```const t = [1, 2, 3, 4, 5]
+    const [first, second, ...rest] = t
+    console.log(first, second)  // 1, 2 is printed
+    console.log(rest)          // [3, 4, 5] is printed ```
+    
+    - Objects: object literals, values of properties can be any type at all. Can be retrieved with "dot" notation.
+    e.g
+
+    ```
+    const object = {
+        name: 'hassan ajaj',
+        age:27,
+        education: 'MSc',
+    }
+    console.log(object1.name) // hassan ajaj is printed
+    ```
+
+    can fill with dot notation or brackets:
+
+        bracket notation allows for space in the property's name.
+    ```
+    object1.address = 'Dublin'
+    object1['Year Of Birth'] = 1995
+    ```
+
+    - Functions
+
+    - Object methods and "this"
+
+    - Classes
--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,28 +1,36 @@
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
+  const part1 = {
+    name: 'Fundementals of React',
+    exercises: 10,
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  }
   return (
     <div>
       <Header course={course}/>
       
-      <Content part={part1} exercies={exercises1} />
+      <Content part={part1.name} exercies={part1.exercises} />
       <br/>
-      <Content part={part2} exercies={exercises2} />
+      <Content part={part2.name} exercies={part2.exercises} />
       <br/>
-      <Content part={part3} exercies={exercises3} />
+      <Content part={part3.name} exercies={part3.exercises} />
       <br/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   );
 }
 
 function Header(props) {
+  console.log(props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -30,6 +38,7 @@ function Header(props) {
   );
 }
 function Content(props) {
+  console.log(props)
   return (
     <div>
       <p>
@@ -41,6 +50,7 @@ function Content(props) {
   )
 }
 function Total(props){
+  console.log(props)
   return (
     <div>
       <p>
@@ -50,6 +60,7 @@ function Total(props){
   )  
 }
 function Part(props){
+  console.log(props)
   return (
     <p>
       {props.part} {props.exercies}
