7f3e927eeb14e01a3989b9ca3f71b5269f75fecc
hassanplz <hassan.albujasim@ucdconnect.ie>
2022-05-18 16:35:07 +0100

    1.4 course information, step4: passing array of objects as props

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,35 +2,29 @@
 function App() {
   const course = 'Half Stack application development'
   
-  const part1 = {
+  const parts = [{
     name: 'Fundementals of React',
     exercises: 10,
   }
-  const part2 = {
+  ,{
     name: 'Using props to pass data',
     exercises: 7,
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14,
   }
+  ]
   return (
     <div>
       <Header course={course}/>
-      
-      <Content part={part1.name} exercies={part1.exercises} />
-      <br/>
-      <Content part={part2.name} exercies={part2.exercises} />
-      <br/>
-      <Content part={part3.name} exercies={part3.exercises} />
-      <br/>
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 }
 
 function Header(props) {
-  console.log(props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -38,34 +32,30 @@ function Header(props) {
   );
 }
 function Content(props) {
-  console.log(props)
   return (
     <div>
-      <p>
-        <Part part={props.part} exercies={props.exercies}/>
-        <Part part={props.part} exercies={props.exercies}/>
-        <Part part={props.part} exercies={props.exercies}/>
-      </p>
+      <p> {props.parts[0].name} {props.parts[0].exercises}</p>
+      <p> {props.parts[1].name} {props.parts[1].exercises}</p>
+      <p> {props.parts[2].name} {props.parts[2].exercises}</p>
     </div>
   )
 }
 function Total(props){
-  console.log(props)
+  const parts = props.parts.map(val => val.exercises)
+
+  const initialValue = 0;
+  const sumWithInitial = parts.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+  );
+
   return (
     <div>
       <p>
-        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+        Number of exercises {sumWithInitial}
       </p>
     </div>
   )  
-}
-function Part(props){
-  console.log(props)
-  return (
-    <p>
-      {props.part} {props.exercies}
-    </p>
-  )
 }
 
 export default App;
