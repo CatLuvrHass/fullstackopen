b7f9f31cdafdca645bb24ac1c60e09a66e767922
Hassan Ajaj <hassan.albujasim@ucdconnect.ie>
2022-06-14 15:40:20 +0100

    1.6 step1 - rerendering reviews

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Display = (props) => (
+  <div>
+    {props.text}
+    {' '}
+    {props.value}
+  </div>
+  
+)
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}  
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h2><strong>Give us feedback</strong></h2>
+      
+      <Button handleClick={()=> setGood(good + 1)} text="Good"/>
+      <Button handleClick={()=> setNeutral(neutral + 1)} text="Neutral"/>
+      <Button handleClick={()=> setBad(bad + 1)} text="Bad"/>
+      
+        <div>
+      
+            <h2><strong>Satistics</strong></h2>    
+            <Display value={good} text='Good'/>
+            <Display value={neutral} text='Neutral'/>
+            <Display value={bad} text='Bad'/>
+        </div>
+ 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
